a479723a5dd3ebeb889c35058d7db9842b13956d
W11 <argani.amh@gmail.com>
2024-03-13 23:09:20 -0400

    add content to contacts and faq page

--- frontend/src/pages/Faq.jsx
+++ frontend/src/pages/Faq.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Faq = () => {
-  return <div>Faq</div>;
+  return (
+    <div className="about-us">
+      <h3 className="title3">Preguntas Frecuentes</h3>
+      <p>
+        Bienvenido a Nourish Net: Conectando Corazones, Alimentos y Esperanza.
+      </p>
+      <p>
+        En Nourish Net, creemos en la fuerza de la comunidad para generar un
+        impacto positivo. Nuestra plataforma digital ha sido diseñada con un
+        propósito claro: reunir a donantes generosos con aquellas personas que
+        necesitan apoyo alimentario, construyendo puentes de solidaridad y
+        compartiendo el amor a través de la comida.
+      </p>
+
+      <p>
+        Nuestro enfoque es simple pero poderoso: facilitar la conexión entre
+        quienes desean dar y quienes necesitan recibir. Desde individuos
+        caritativos hasta organizaciones benéficas, Nourish Net ofrece una
+        plataforma inclusiva donde todos pueden contribuir de manera
+        significativa.
+      </p>
+    </div>
+  );
 };
 
 export default Faq;
