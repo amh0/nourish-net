2e22e8f4c4ce9936b885290ac1bcf61072493838
W11 <argani.amh@gmail.com>
2024-03-16 18:53:01 -0400

    add styles to footer component

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "./Footer.css";
+import {
+  Heart,
+  FacebookLogo,
+  InstagramLogo,
+  TiktokLogo,
+} from "@phosphor-icons/react";
 const Footer = () => {
   return (
     <div className="footer">
       <div className="description-section">
         <div className="logo-section">
-          <div></div>
+          <Heart size={48} color="#ffffff" weight="light" />
           <h3 className="title3">NourishNet</h3>
         </div>
         <p className="parr1">
@@ -13,25 +20,30 @@ const Footer = () => {
           solidaridad con la necesidad{" "}
         </p>
       </div>
+      <div className="footer-filler"></div>
       <div className="links-section">
         <h5 className="title5">Conoce más</h5>
-        <div className="links-wrapper">
-          <ol>
-            <li>Inicio</li>
-            <li>Nosotros</li>
-            <li>Contacto</li>
-            <li>Preguntas Frecuentes</li>
-          </ol>
-        </div>
+        <ol className="links-wrapper">
+          <li>
+            <Link className="link">Inicio</Link>
+          </li>
+          <li>
+            <Link className="link">Nosotros</Link>
+          </li>
+          <li>
+            <Link className="link">Contacto</Link>
+          </li>
+          <li>
+            <Link className="link">Preguntas Frecuentes</Link>
+          </li>
+        </ol>
       </div>
       <div className="socials-section">
         <h5 className="title5">Nuestras redes</h5>
         <div className="socials-wrapper">
-          <ol>
-            <div className="icon-placeholder"></div>
-            <div className="icon-placeholder"></div>
-            <div className="icon-placeholder"></div>
-          </ol>
+          <FacebookLogo size={48} color="#ffffff" weight="light" />
+          <InstagramLogo size={48} color="#ffffff" weight="light" />
+          <TiktokLogo size={48} color="#ffffff" weight="light" />
         </div>
       </div>
     </div>
